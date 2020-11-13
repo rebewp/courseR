8a65befa6125f9486cc054fcc4455a5ee1b2fbce
rebewp <rebewp@icloud.com>
2020-11-13 15:41:34 +0100

    7b ready

--- assignment1.R
+++ assignment1.R
@@ -7,4 +7,14 @@ named_farmers_animals
 farmers_animals_2 <- c(chicken = 53323, cows = 1334, horses = 4323)
 farmers_animals_2
 # more: 75% chicken, 30% cows, 50% horses ; absolute numbers; rounding with ceiling()
+chicken <- farmers_animals[1]
+chicken <- ceiling(chicken + chicken * 0.75)
+chicken
 
+cows <- farmers_animals[2]
+cows <- ceiling(cows + cows * 0.3)
+cows
+
+horses <- farmers_animals[3]
+horses <- ceiling(horses + horses * 0.5)
+horses
