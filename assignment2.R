7918f07e3359bedc58e2f7ce3c3d170a3ecd9caa
rebewp <rebewp@icloud.com>
2020-11-27 18:05:23 +0100

    testing publisher reading two sheets at once

--- assignment2.R
+++ assignment2.R
@@ -1,24 +1,79 @@
 #Assignment 2
 ##reading & tidying data
 
-#loading tidyverse just to get the included functions, don't know if its necessary yet
-library(tidyverse) 
+#package loading
 install.packages("readxl")
 library(readxl)
+library(tidyverse) 
 
+#reading in data
 
-books_tsv <- read_tsv("datasets/books.tsv") # because of the .tsv we need to use read_tsv
+books_tsv <- read_tsv("datasets/books.tsv") 
+#separating authors
+books_tsv_tidy <- books_tsv %>% 
+  separate(author, into = c("author_1", "author_2", "author_3"), sep = " and ")
+ 
 
 books_txt <- read.delim("datasets/books.txt", sep = "|") 
-# in this case we use read and its included function delim. 
-# Calling it with the command read.delim and we need to ad the sign the data uses to separate single lines,
-# in this case its "|". When you open up the books.txt file in a texteditor you easily find out the so called seperator
+#separating authors
+books_txt_tidy <- books_txt %>% 
+  separate(author, into = c("author_1", "author_2", "author_3"), sep = " and ")
 
-ches_2017 <- read_csv("datasets/ches_2017.csv")#basically the same as .tsv
+
+
+ches_2017 <- read_csv("datasets/ches_2017.csv")
 ches_2017_modified <- read_csv("datasets/ches_2017_modified.csv", skip = 4)
-# the first 4 lines are not containing anything 
-#so with skip we tell are to skip those
 
-publishers <- read_xlsx("datasets/publishers.xlsx")# we first have to install and load the readrxl package 
+ches_2017_modified_tidy <- ches_2017_modified %>% 
+  pivot_wider(names_from = variable, values_from = value)
 
-spotify2018 <- read_csv("datasets/spotify2018.csv")# just as the first .csv file
+
+publishers <- read_xlsx("datasets/publishers.xlsx")
+
+
+# Using excel_sheets, we can get the names of sheets and create a list with them.
+publisher_sheets <- readxl::excel_sheets("datasets/publishers.xlsx")
+publisher_sheets <- as.list(publisher_sheets)
+
+# When we have the list, we createa loop for each sheet and we save the data in the variable "finalData".
+
+## testtesttest
+finalData <- data.frame()
+for (i in 1:length(sheets)){
+  if (nrow(finalData) == 0 ) {
+    data <- read_excel("example.xlsx", sheet = sheets[[i]] )
+    finalData <- data
+  }else{
+    dataNext <- read_excel("example.xlsx", sheet = sheets[[i]] )
+    finalData <-  cbind(finalData,  dataNext)
+  }
+}
+finalData
+
+publishers <- tibble()
+for (i in 1:length(publisher_sheets)){
+  if (nrow(publishers) == 0){
+    data <- read_excel("datasets/publishers.xlsx", sheet = publisher_sheets[[i]])
+    publishers <- data
+  }else{
+    dataNext <- read_excel("datasets/publishers.xlsx", sheet = publisher_sheets[[i]])
+    publishers <- cbind(publishers, dataNext)
+  }
+}
+
+read_excel_allsheets <- function(filename, tibble = TRUE) {
+  # I prefer straight data.frames
+  # but if you like tidyverse tibbles (the default with read_excel)
+  # then just pass tibble = TRUE
+  sheets <- readxl::excel_sheets(filename)
+  x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
+  if(!tibble) x <- lapply(x, as.data.frame)
+  names(x) <- sheets
+  x
+}
+
+publishers <- read_excel_allsheets("datasets/publishers.xlsx")
+bind_rows(publishers)
+
+
+spotify2018 <- read_csv("datasets/spotify2018.csv")
