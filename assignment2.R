e6357a10a6d30a668355496de9d4bba7ca05609f
rebewp <rebewp@icloud.com>
2020-11-27 15:42:12 +0100

    publisher loaded

--- assignment2.R
+++ assignment2.R
@@ -3,6 +3,9 @@
 
 #loading tidyverse just to get the included functions, don't know if its necessary yet
 library(tidyverse) 
+install.packages("readxl")
+library(readxl)
+
 
 books_tsv <- read_tsv("datasets/books.tsv") # because of the .tsv we need to use read_tsv
 
@@ -12,4 +15,9 @@ books_txt <- read.delim("datasets/books.txt", sep = "|")
 # in this case its "|". When you open up the books.txt file in a texteditor you easily find out the so called seperator
 
 ches_2017 <- read_csv("datasets/ches_2017.csv")#basically the same as .tsv
-ches_2017_modified <- read_csv("datasets/ches_2017_modified.csv", skip = 4)# the first 4 lines are not containing anything so with skip we tell are to skip those
+ches_2017_modified <- read_csv("datasets/ches_2017_modified.csv", skip = 4)
+# the first 4 lines are not containing anything 
+#so with skip we tell are to skip those
+
+publishers <- read_xlsx("datasets/publishers.xlsx")# we first have to install and load the readrxl package 
+
