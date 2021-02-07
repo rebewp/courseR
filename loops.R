108bb47df6b60186191890665508e74b6c4959b1
rebewp <rebewp@icloud.com>
2021-02-07 09:38:45 +0100

    more for and if

--- loops.R
+++ loops.R
@@ -40,9 +40,42 @@ walk(v, print)
 #} else {
 #do_something_else
 #}
+y <- sample(10, 1)
+y
 set.s
 set.seed(123)
 x <- sample(10, 1)
 x
-x
-x
+seq_along(example_strings)
+print(example_strings[1])
+print(example_strings[seq_along(example_strings)[1]])
+
+#car example
+cars_2 <- mtcars %>% 
+  select(mpg, cyl, disp, hp, gear)
+
+cars_tbl <- mtcars %>% 
+  rownames_to_column(var = "model_name") %>% 
+  select(mpg, cyl, disp, hp, gear)
+
+#leeren vector für bearbeitung generieren
+#column names setzen
+output <- double(length = ncol(cars_tbl))
+output <- set_names(output, colnames(cars_tbl))
+output
+
+#durchschnitt der column inhalte von cars_tbl in output schreiben
+for (i in seq_along(cars_tbl)) {
+  output[[i]] <- mean(cars_tbl[[i]])
+}
+
+
+glimpse(cars_tbl)
+head(cars_tbl)
+head(cars_2)
+?rownames_to_column
+has_rownames(mtcars)
+has_rownames(cars_2)
+mtcars_tbl <- rownames_to_column(mtcars, var = "car") %>% as_tibble()
+mtcars_tbl
+has_rownames(mtcars)
