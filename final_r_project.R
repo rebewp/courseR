cf92ec10685ecd1942cf6f053ef4b457e5e74197
rebewp <rebewp@icloud.com>
2021-04-06 14:12:13 +0200

    more and more done

--- final_r_project.R
+++ final_r_project.R
@@ -3,14 +3,18 @@ install.packages("COVID19")
 library(tidyverse)
 library(COVID19)
 library(rvest)
+library(ggplot2)
+library(lubridate)
 
 ?covid19
 
+sum(1:2)
+
 #getting data
 city <- covid19(c("Germany"), level = 3)
-x <- covid19()
-us_covid_base_lvl_3 <- covid19(c("US"), end = "2021-02-01", level = 3)
-us_covid_base_lvl_2 <- covid19(c("US"), end = "2021-02-01", level = 2)
+x <- covid19(raw = FALSE)
+us_covid_base_lvl_3 <- covid19(c("US"), end = "2020-12-31", level = 3)
+us_covid_base_lvl_2 <- covid19(c("US"), end = "2020-12-31", level = 2)
 
 #saving locally
 save(us_covid_base_lvl_2, file = "us_covid_base_lvl_2.Rdata")
@@ -54,8 +58,58 @@ tidy_us_covid_base_lvl_2 <- us_covid_base_lvl_2 %>%
   rename(State = administrative_area_level_2) %>% 
   inner_join(wiki_governors, by = "State")
 
+us_covid_cases_party <- tidy_us_covid_base_lvl_2 %>% 
+  select(date, State, confirmed, Party) %>% 
+  mutate(month = month(date), 
+         year = year(date),
+         day = day(date)) %>% 
+  group_by(month, year, Party)
+
+jan_data <- filter(us_covid_cases_party, month == 1,day == 31) %>% 
+  summarise(cases = sum(confirmed, na.rm= TRUE))
+feb_data <- filter(us_covid_cases_party, month == 2,day == 28) %>% 
+  summarise(cases = sum(confirmed, na.rm= TRUE))
+march_data <- filter(us_covid_cases_party, month == 3,day == 31) %>% 
+  summarise(cases = sum(confirmed, na.rm= TRUE))
+april_data <- filter(us_covid_cases_party, month == 4,day == 30) %>% 
+  summarise(cases = sum(confirmed, na.rm= TRUE))
+may_data <- filter(us_covid_cases_party, month == 5,day == 31) %>% 
+  summarise(cases = sum(confirmed, na.rm= TRUE))
+june_data <- filter(us_covid_cases_party, month == 6,day == 30) %>% 
+  summarise(cases = sum(confirmed, na.rm= TRUE))
+july_data <- filter(us_covid_cases_party, month == 7,day == 31) %>% 
+  summarise(cases = sum(confirmed, na.rm= TRUE))
+august_data <- filter(us_covid_cases_party, month == 8,day == 31) %>% 
+  summarise(cases = sum(confirmed, na.rm= TRUE))
+september_data <- filter(us_covid_cases_party, month == 9,day == 30) %>% 
+  summarise(cases = sum(confirmed, na.rm= TRUE))
+october_data <- filter(us_covid_cases_party, month == 10,day == 31) %>% 
+  summarise(cases = sum(confirmed, na.rm= TRUE))
+november_data <- filter(us_covid_cases_party, month == 11,day == 30) %>% 
+  summarise(cases = sum(confirmed, na.rm= TRUE))
+december_data <- filter(us_covid_cases_party, month == 12,day == 31) %>% 
+  summarise(cases = sum(confirmed, na.rm= TRUE))
+
+complete_data <- rbind(jan_data, feb_data, march_data, april_data, may_data, 
+                       june_data, july_data, august_data, september_data, 
+                       october_data, november_data, december_data)
 
 
 
 
+
+
+
+month(us_covid_cases_party$date)
+####### visualization
+
+covid_cases_per_state_plot <- ggplot(complete_data,
+                                     aes(x = cases, 
+                                         y = month, 
+                                         color = Party)) +
+  geom_point() +
+  coord_flip() +
+  ggtitle("US Covid Cases Per State And Party") +
+  theme_bw()
+covid_cases_per_state_plot
 
